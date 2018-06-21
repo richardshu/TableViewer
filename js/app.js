7e16172bc00946dfba4e0e265aa8705a9a6b211d
Richard <richardjshu@gmail.com>
2018-06-21 12:50:33 -0700

    Style the active link

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Set the default landing page to the round table
+// Set the default landing page to the round tables link
 if (!location.hash) {
 	location.hash = "#round";
 }
@@ -10,6 +10,7 @@ function updateContent() {
 	const tableSizeContainer = document.getElementById("table-size-container");
 	var fragmentId = location.hash.substr(1);
 	tableSizeContainer.innerHTML = getContent(fragmentId);
+	setActiveLink(fragmentId);
 };
 
 // Returns content depending on the current fragment identifier
@@ -20,6 +21,23 @@ function getContent(fragmentId) {
 		rectangle: "This is the rectangle tables page"
 	};
 	return content[fragmentId];
+}
+
+
+// Sets the "active" class on the active navigation link
+function setActiveLink(fragmentId) {
+	const tableShapeLinks = document.getElementById("navbar").children;
+	for (var i = 0; i < tableShapeLinks.length; i++) {
+		var link = tableShapeLinks[i];
+		var pageName = link.getAttribute("href").substr(1);
+		if (pageName === fragmentId) {
+			link.setAttribute("class", "active");
+		}
+		else {
+			link.removeAttribute("class", "active");
+		}
+		console.log(link);
+	}
 }
 
 window.addEventListener("hashchange", updateContent);
